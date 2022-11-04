f7ac7bdb665a377f4c29194303580c2334ef747d
Seednode <git@seedno.de>
2022-11-04 16:55:42 -0500

    Removed error logging from Execute(), redundant since Args: function already handles

--- cmd/root.go
+++ cmd/root.go
@@ -46,11 +46,7 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	rootCmd.Execute()
 }
 
 func init() {
--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.4.5"
+var Version = "0.4.6"
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
